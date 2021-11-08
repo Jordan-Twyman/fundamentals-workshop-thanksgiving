7a283d9bca4a5f8a9f378b67f873806aeff9f032
Jaytwy24 <jtwy24@gmail.com>
2021-11-08 16:04:33 -0500

    last commit of the day working on filter

--- src/scripts/PartyNameFilter.js
+++ src/scripts/PartyNameFilter.js
@@ -1,8 +1,29 @@
+import { GuestsList } from "./PartyList";
+import { getGuests, useGuests } from "./PartyProvider";
+
 const filterNames = document.querySelector('.name-filer');
 const contentTarget = document.querySelector("body");
 
 export const filterGuests = () => {
-    filterNames.innerHTML = `
+
+    getGuests()
+    .then(() => {
+        const partyGuests = useGuests()
+        render(partyGuests)
+    })
+}
+
+eventHub.addEventListener("change", (eventObj) => {
+    if (eventObj.target.id === "guestSelect") {
+        const filterObject = {
+            type: "Guests",
+            filterValue: eventObj.target.value
+        }
+        GuestsList(filterObject)
+    }
+})
+const render = dishCollection => {
+filterNames.innerHTML = `
     
-    `
+`
 }
